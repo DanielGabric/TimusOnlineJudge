cf3e625ae98a2d197a867771956077133c16b0e9
Dan <danielgabric@live.com>
2015-05-18 15:11:40 -0400

    .

--- 1353.py
+++ 1353.py
@@ -1,7 +1,10 @@
 F = [[0 for i in range(82)] for i in range(10)]
 F[0][0]=1
+#lengths of numbers from 1-9
 for i in range(1,10):
+    #sums from 1 to 81
     for s in range(1,82):
+        #digits from 0-9
         for k in range(0,10):
             if s >=k:
                 F[i][s]+=F[i-1][s-k]
