58686ea880bf1e28bd895f6819e3e63c0438facd
Dan <danielgabric@live.com>
2015-05-28 15:10:57 -0400

    bottom-up DP

--- 1012.py
+++ 1012.py
@@ -2,9 +2,8 @@
 N = input()
 K = input()
 F = [[0 for i in range(0,K)] for i in range(0, N+1)]
-for i in range(0,1):
-    for j in range(1,K):
-        F[i][j]=1
+for j in range(1,K):
+    F[0][j]=1
 for i in range(1,N+1):
     for j in range(0,K):
         for k in range(0,K):
