6a8c850f6389f0fc8ddb1938f06f107c171de963
Dan <danielgabric@live.com>
2015-05-28 01:15:36 -0400

    bottom-up DP solution

--- 1009.py
+++ 1009.py
@@ -3,7 +3,7 @@ def rec(N,K,lastDigit=0):
     sums=0
     if N==0:
         return 1
-    h = str(N)+str(K)+str(lastDigit)
+    h = str(N)+str(lastDigit)
     if h in cache:
         return cache[h]
     if lastDigit != 0:
@@ -12,8 +12,6 @@ def rec(N,K,lastDigit=0):
         sums+=rec(N-1,K,i)
     cache[h]=sums
     return sums 
-N=0
-K=0
 N=input()
 K = input()
 print rec(N,K)
---
+++ 1012.py
@@ -0,0 +1,14 @@
+#smarter bottom-up DP solution than previous memoization top down solution 
+N = input()
+K = input()
+F = [[0 for i in range(0,K)] for i in range(0, N+1)]
+for i in range(0,1):
+    for j in range(1,K):
+        F[i][j]=1
+for i in range(1,N+1):
+    for j in range(0,K):
+        for k in range(0,K):
+            if k==0 and j==0:
+                continue
+            F[i][j]+=F[i-1][k]
+print F[N][K-1]
