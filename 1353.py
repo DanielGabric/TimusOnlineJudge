bcaa5ce48be99ccba04c55baf6bc356e2cf0c7a5
Dan <danielgabric@live.com>
2015-05-28 15:47:45 -0400

    bottom-up DP

--- 1353.py
+++ 1353.py
@@ -1,7 +1,7 @@
 #solve using bottom-up Dynamic Programming
 #solve from smaller sums then combine them
 #to solve the bigger problem
-F = [[0 for i in range(82)] for i in range(10)]
+F = [[0 for i in range(10)] for i in range(82)]
 F[0][0]=1
 #lengths of numbers from 1-9
 for i in range(1,10):
@@ -10,10 +10,8 @@ for i in range(1,10):
         #digits from 0-9
         for k in range(0,10):
             if s >=k:
-                F[i][s]+=F[i-1][s-k]
-sums=0
+                F[s][i]+=F[s-k][i-1]
 L = input()
-for i in range(1,10):
-    sums+=F[i][L]
+sums = sum(F[L])
 if L == 1: sums+=1
 print sums 
